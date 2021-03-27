c997d81b6e59c4c0c43419c5080bb1ce4bdcae05
JaaO21 <jorgeandres2104@gmail.com>
2021-03-26 23:56:30 -0500

    feat app controler message hello

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { AppController } from "./app.controller";
 import { CommonModule } from "./modules/@common/common.module";
 import appConfig from "./modules/@common/config/app.config";
 import typeormConfig from "./modules/@common/config/typeorm.config";
@@ -20,7 +21,8 @@ import typeormConfig from "./modules/@common/config/typeorm.config";
       name: 'user'
     }),
     CommonModule
-  ]
+  ],
+  controllers:[AppController]
 })
 
 export class AppModule{}
