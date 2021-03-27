b46a946dc33d5b166203cdd6a8a49e74a91f00cc
JaaO21 <jorgeandres2104@gmail.com>
2021-03-27 00:06:20 -0500

    feat appService

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
 import { CommonModule } from "./modules/@common/common.module";
 import appConfig from "./modules/@common/config/app.config";
 import typeormConfig from "./modules/@common/config/typeorm.config";
@@ -22,7 +23,8 @@ import typeormConfig from "./modules/@common/config/typeorm.config";
     }),
     CommonModule
   ],
-  controllers:[AppController]
+  controllers:[AppController],
+  providers:[AppService]
 })
 
 export class AppModule{}
